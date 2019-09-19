903a20a17ea52f59cd96c81c5d73f3d7ed89c12e
Marcos Benasayag <marcosbenasayag@Pablos-Mac-Mini.local>
2019-09-18 23:48:11 -0300

    first commit

---
+++ src/js/const.js
@@ -0,0 +1,14 @@
+export const APP_ID = 'CCAACE3E-2A55-43B6-9D2A-3B1CEFF9E02B';
+export const USER_ID = 'DrTest';
+export const DISPLAY_NONE = 'none';
+export const DISPLAY_BLOCK = 'block';
+export const DISPLAY_FLEX = 'flex';
+export const ACTIVE_CLASSNAME = 'active';
+export const KEY_ENTER = 13;
+export const FILE_ID = 'attach_file_id';
+export const UPDATE_INTERVAL_TIME = 5 * 1000;
+export const COLOR_RED = '#DC5960';
+export const MESSAGE_REQ_ID = 'reqId';
+export const OPEN_CHANNEL_SEARCH_URL = 'search_open_channel';
+
+export const body = document.querySelector('body');
